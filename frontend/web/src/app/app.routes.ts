edf922f9d62e00ad0f52b9dbe1fbecd8c3213729
hamza <hamza.elaloui10@gmail.com>
2024-10-30 10:43:28 +0100

    add sign up form

--- frontend/web/src/app/app.routes.ts
+++ frontend/web/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent }, 
+    { path: 'sign-up', component: SignupComponent }, 
+];
+
+
---
+++ frontend/web/src/app/signup/signup.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.css'
+})
+export class SignupComponent {
+
+}
