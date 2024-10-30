be8dd55962e46a0b318070c749950356dd5d3c89
hamza <hamza.elaloui10@gmail.com>
2024-10-30 11:14:33 +0100

    add sign in page

--- frontend/web/src/app/app.routes.ts
+++ frontend/web/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { SignupComponent } from './signup/signup.component';
 import { HomeComponent } from './home/home.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent }, 
     { path: 'sign-up', component: SignupComponent }, 
+    { path: 'sign-in', component: SignInComponent },
 ];
 
 
